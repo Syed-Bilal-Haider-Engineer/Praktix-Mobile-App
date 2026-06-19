7164867f9d96f97c09e543654c935856362f8cee
syed bilal haider <bilaldev151214@gmail.com>
2026-06-19 03:05:40 +0200

    add menifest.json and plugin registrant

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,10 +1,12 @@
-//
-//  Generated file. Do not edit.
-//
+
 
 import FlutterMacOS
 import Foundation
 
+import shared_preferences_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
